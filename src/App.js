e64ab3970785f7024b761ff86ca0338eb5e5ac93
Alex Gaw <alexanderjgaw@gmail.com>
2017-01-18 21:06:18 -0600

    First react thingy with some stuff I'm sorry my commit is so poorly named but at least its unique

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Hello from './Hello';
 
 class App extends Component {
   render() {
+
+    const users = [
+      { _id: 1, name: 'Leia' },
+      { _id: 2, name: 'Luke' },
+      { _id: 3, name: 'Han' }
+    ];
+
     return (
       <div className="App">
         <div className="App-header">
@@ -11,6 +19,9 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
+          <div>
+            { users.map( user => < Hello key={user._id} name={user.name} />)}
+          </div>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
