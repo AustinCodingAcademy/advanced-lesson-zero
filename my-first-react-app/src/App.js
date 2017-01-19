35bbbe3657abd9b1561e8243409566757d641d42
chrisperk <christopherperkins.512@gmail.com>
2017-01-18 20:41:15 -0600

    Hello World v2with modularized components

---
+++ my-first-react-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './Hello.js';
+
+class App extends React.Component {
+    render() {
+        const users = [
+            { _id: 1, name: 'Leia' },
+            { _id: 2, name: 'Luke' },
+            { _id: 3, name: 'Han' }
+        ];
+
+        return (
+            <div>
+                {
+                    users.map(function(user) {
+                        return <Hello key={user._id} name={user.name} />;
+                    })
+                }
+            </div>
+        );
+    }
+}
+
+export default App;
--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-const App = function() {
-    return (
-        <div>
-            <h1>Hello React!</h1>
-            <img src={require('./img/es6complete.png')} role="presentation" />
-        </div>
-    );
-}
+import App from './App.js';
 
 ReactDOM.render(
   <App />,
