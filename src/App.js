bb288915117c761d080225512272cb5acbd40680
keat334 <hodges.keaton@gmail.com>
2017-01-18 20:43:21 -0600

    updated

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import add from './add';
+
+class HelloWorld extends Component {
+  render () {
+    return (
+    <div className="hello-world">
+    Hello World, {this.props.name}!
+    </div>
+);
+  }
+}
+
+
 
 class App extends Component {
   render() {
+
+    // Calculate sum from import
+    const sum = add(200, 300);
+
+    const users = [
+     { _id: 1, name: 'Leia' },
+     { _id: 2, name: 'Luke' },
+     { _id: 3, name: 'Han' }
+   ];
+
+
     return (
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+          {users.map((user) => {
+            return (
+              <div>
+              <HelloWorld name={user.name} key={user._id} />
+                </div>
+            );
+          })}
+
+
+
+      </div>
       </div>
     );
   }
