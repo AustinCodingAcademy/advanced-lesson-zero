67e543f16d9997758a5e323c8b6506e1f234d6c7
chrisperk <christopherperkins.512@gmail.com>
2017-01-19 13:27:32 -0600

    Refactored Hello.js to be functional component instead of class

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import Hello from './Hello.js';
 
 class App extends React.Component {
@@ -13,7 +12,7 @@ class App extends React.Component {
         return (
             <div>
                 {
-                    users.map(function(user) {
+                    users.map((user) => {
                         return <Hello key={user._id} name={user.name} />;
                     })
                 }
--- my-first-react-app/src/Hello.js
+++ my-first-react-app/src/Hello.js
@@ -1,12 +1,9 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-    render() {
-        return (
-            <h1>Hello {this.props.name}!</h1>
-        );
-    }
+const Hello = (props) => {
+    return (
+        <h1>Hello {props.name}!</h1>
+    );
 }
 
 export default Hello;
