df4cd31ce46ce8d723d3867448325eff8fb9dfd7
nicolemichelle88 <daugereaux@gmail.com>
2017-01-22 13:01:53 -0600

    lesson one hw

--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -2,10 +2,30 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-const App = function(){
-  return(
-    <h1>Hello React!</h1>
-  );
+class Hello extends React.Component {
+  render() {
+    return <h1>Hello {this.props.name}!</h1>;
+  }
+}
+
+class App extends React.Component {
+  render() {
+    const users = [
+      { _id: 1, name: 'Leia' },
+      { _id: 2, name: 'Luke' },
+      { _id: 3, name: 'Han' }
+    ];
+
+    return (
+      <div>
+        {
+          users.map(function(user) {
+            return <Hello key={user._id} name={user.name} />;
+          })
+        }
+      </div>
+    );
+  }
 }
 
 
