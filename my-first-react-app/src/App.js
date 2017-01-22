a08f6363ebce9211d309d779bea3469665188051
zekeg <zechariah.guadiana@gmail.com>
2017-01-22 05:02:43 -0600

    Homework 1: Rendering and Module Passing

--- my-first-react-app/src/App.js
+++ my-first-react-app/src/App.js
@@ -1,21 +1,36 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+//we need to import the first component to use react
+import React from 'react';
+import Hello from './Hello.js'
+//Here is our app
+class App extends React.Component {
+    render()
+    {
+        const users = [{
+                _id: 1,
+              name: 'Leia'
+            },
+            {
+                _id: 2,
+                name: 'Luke'
+            },
+            {
+                _id: 3,
+                name: 'Han'
+            }
+        ];
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+        return ( <
+            div > {
+                users.map(function(user) {
+                    return < Hello key={user._id}
+                    name={
+                        user.name
+                    }
+                    />;
+                })
+            } <
+            /div>
+        );
+    }
 }
-
-export default App;
+module.exports = App;
--- my-first-react-app/src/index.js
+++ my-first-react-app/src/index.js
@@ -1,9 +1,9 @@
+//we need to import the first component to use react
+//we need the second component to run the virtual dom and to render()
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import App from './App.js';
+//Here is a react component named Hello
+//we must first define it as a react component to access library features
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-);
+ReactDOM.render( < App / > , document.getElementById('root'));
