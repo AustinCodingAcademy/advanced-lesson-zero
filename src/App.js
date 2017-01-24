e2d4594ab3cae630fe5c9e9e1354420643a4e8e9
Joe King <realkalel@yahoo.com>
2017-01-23 18:56:25 -0600

    second commit

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Hello from './helloworld';
+import Goodbye from './Goodbye';
+
 class App extends Component {
   render() {
     return (
       <div className="App">
+     <Hello/>
+     <Goodbye/>
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
---
+++ src/Goodbye.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+class Goodbye extends React.Component {
+  render() {
+    return <h1>Goodbye {this.props.name}!</h1>;
+  }
+}
+export default Goodbye;
